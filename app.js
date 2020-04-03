a8278c0a184164063756bc5f98ff6fed62cf82df
성현제 <hyunjaesung@seonghyeonje.local>
2020-04-03 17:37:21 +0900

    1. 변수

---
+++ app.js
@@ -0,0 +1,18 @@
+//VARIABLES
+
+let isEmployed = true;
+let hasApartment = false; //Boolean
+
+let firstName = "Filip";
+let lastName = "Jerga"; //String
+let age = 29; //Number
+
+let birthday; //undefined
+
+let phone = null; //Null
+
+console.log("Name: " + firstName + " " + lastName + " age: " + age);
+
+console.log(birthday);
+
+console.log(phone);
