8fcf1b0a883a6614d7f95d039cf10eb6961f4b6d
성현제 <hyunjaesung@seonghyeonje.local>
2020-04-03 18:25:12 +0900

    2. 함수

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 //VARIABLES
+// debugger;
+// let isEmployed = true;
+// let hasApartment = false; //Boolean
 
-let isEmployed = true;
-let hasApartment = false; //Boolean
+//String
+// let age = 29; //Number
 
-let firstName = "Filip";
-let lastName = "Jerga"; //String
-let age = 29; //Number
+// let birthday; //undefined
 
-let birthday; //undefined
+// let phone = null; //Null
 
-let phone = null; //Null
+// let fullName = "Filip Jerga";
 
-console.log("Name: " + firstName + " " + lastName + " age: " + age);
+// fullName = "John Green";
 
-console.log(birthday);
+// console.log(fullName);
 
-console.log(phone);
+// console.log("Name: " + firstName + " " + lastName + " age: " + age);
+
+// console.log(birthday);
+
+// console.log(phone);
+
+function runApp() {
+  debugger;
+  const firstName = "Filip";
+  const lastName = "Jerga";
+
+  const firstName2 = "John";
+  const lastName2 = "Green";
+
+  function sayHello(name, lname) {
+    console.log("Hello " + name + " " + lname);
+  }
+
+  sayHello(firstName, lastName);
+  sayHello(firstName2, lastName2);
+  sayHello("Martha", "Doppler");
+}
+
+runApp();
