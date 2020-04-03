363211f8eca74f82ba29061767a52db3a4b0f866
성현제 <hyunjaesung@seonghyeonje.local>
2020-04-03 18:55:40 +0900

    4. var let const

--- app.js
+++ app.js
@@ -21,25 +21,43 @@
 // console.log(birthday);
 
 // console.log(phone);
+
+//var
+// function scope
+// undefined in case i am accesing variable before declaration
+
+//let
+//block scope
+//ReferenceError in case i am accesing variable before declaration
+
+//const
+//block
+//ReferenceError in case i am accessing variable before declaration
+//const assign value only once
+
 let scope = "SCOPE!!!!";
 
 function hello() {
   console.log("Hello World");
 }
 
+//Hoisting
 function runApp() {
-  debugger;
-  hello();
+  console.log(firstName);
   const firstName = "Filip";
   const lastName = "Jerga";
 
   const firstName2 = "John";
   const lastName2 = "Green";
 
+  if (true) {
+    var fScope = "Function Scope!";
+    let bScope = "Block Scope";
+  }
+
+  console.log(fScope);
   function sayHello(name, lname) {
-    const firstName3 = "현제";
-    hello();
-    console.log(firstName3);
+    console.log("hello" + name + " " + lname);
   }
 
   sayHello(firstName, lastName);
