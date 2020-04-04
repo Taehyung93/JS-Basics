d11628da169d70b2f2935ad20fb09e2641a5257f
성현제 <hyunjaesung@seonghyeonje.local>
2020-04-04 10:25:54 +0900

    6. 객

--- app.js
+++ app.js
@@ -3,35 +3,33 @@ function runApp() {
   var firstName = "Filip";
   const lastName = "Jerga";
 
-  const brand = "mercedes";
-  const type = "benz";
-  const produceYear = 1995;
+  const car = {
+    brand: "mercedes",
+    type: "benz",
+    produceYear: 1995,
+    displayInfo: function() {
+      console.log(`${this.brand} ${this.type} - ${this.produceYear}`);
+    }
+  };
 
-  const brand2 = "hyundai";
-  const type2 = "sonata";
-  const produceYear2 = 1987;
+ 
 
-  const brand3 = "ferrari";
-  const type3 = "unknown";
-  const produceYear3 = 1970;
-
-  const brand4 = "mitshubishi";
-  const type4 = "lancer";
-  const produceYear4 = 2002;
+  const car2 = {
+    brand: "mitshubishi",
+    type: "lancer",
+    produceYear: 2002
+  };
 
   const firstName2 = "John";
   const lastName2 = "Green";
 
   //display car Info
   //params: brand, type, year
-  function displayCarInfo(brand, type, year) {
-    console.log(`${brand} - ${type} - ${year}`);
+  function displayCarInfo(car) {
+    console.log(`${car.brand} - ${car.type} - ${car.produceYear}`);
   }
 
-  displayCarInfo(brand, type, produceYear);
-  displayCarInfo(brand2, type2, produceYear2);
-  displayCarInfo(brand3, type3, produceYear3);
-  displayCarInfo(brand4, type4, produceYear4);
+  car.displayInfo(car);
 }
 
 runApp();
