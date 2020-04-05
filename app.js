60847ccd0d4aa80a831f585695d9481db6fb20ff
성현제 <hyunjaesung@seonghyeonje.local>
2020-04-05 14:59:35 +0900

    9. 임포팅, 프로토타입

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
-class Car {
-  brand = "";
-  type = "";
-  produceYear = null;
-
-  constructor(brand, type, produceYear)
-
-  displayInfo() {
-    console.log(`${this.brand} ${this.type} - ${this.produceYear}`);
-  }
-}
+const Car = require("./Car");
 
 function runApp() {
-  const car = new Car();
+  const car = new Car("mercedes", "benz", 1995);
+  const car2 = new Car("mitshubishi", "lancer", 2002);
 
-//   car.brand = "mercedes";
-//   car.type = "benz";
-//   car.produceYear = 1995;
-
-  const car2 = {
-    brand: "mitshubishi",
-    type: "lancer",
-    produceYear: 2002,
-    displayInfo: function() {
-      console.log(`${this.brand} ${this.type} - ${this.produceYear}`);
-    }
-  };
   car.displayInfo();
   car2.displayInfo();
+
+  console.log(Car.prototype === car.__proto__);
+  console.log(Car.prototype === car2.__proto__);
 }
 
 runApp();
